4f32c8ae8b72317fe088e659bd2e47bb19506f6d
Stanley Fils <stanleyfils@gmail.com>
2020-06-29 23:47:59 -0400

    Update code

--- calculator.js
+++ calculator.js
@@ -34,4 +34,10 @@ for (let i = 0; i < operator.length; i++) {
   operator[i].addEventListener("click", function () {
     alert("the operator clicked:" + this.id);
   });
+
+let number = document.getElementsByClassName("number");
+for (let i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function () {
+    alert("the number clicked:" + this.id);
+  });
 }
